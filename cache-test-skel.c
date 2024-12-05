6357a7ee5726c0060366c8ac30bb4f94d9f73cb4
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2024-12-04 23:27:54 -0800

    plss

--- cache-test-skel.c
+++ cache-test-skel.c
@@ -54,15 +54,16 @@ int get_cache_assoc(int size) {
   flush_cache();
 
   access_cache(0);
-  while (access_cache(0)) {
-    assoc++;
-    flush_cache();
-
-    for (int i = 1; i <= assoc; i++)
+  while (access_cache(0))
+  {
+    for (int i = 0; i < assoc; i++)
     {
         access_cache(i * size);
     }
+    assoc++;
+    flush_cache();
   }
+  
   return assoc;
 }
 
