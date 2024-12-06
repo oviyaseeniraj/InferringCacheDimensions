869a2b8f614339ede6c1483144a04ee8dabb7ec5
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2024-12-05 19:01:22 -0800

    4tfwhrgwrgwrg

--- cache-test-skel.c
+++ cache-test-skel.c
@@ -42,7 +42,6 @@ int get_cache_size(int block_size) {
     {
       //access_cache(cache_size);
       cache_size += block_size;
-      printf("cache_size: %d\n", cache_size);
       access_cache(cache_size);
       // if (!access_cache(cache_size))
       // {
